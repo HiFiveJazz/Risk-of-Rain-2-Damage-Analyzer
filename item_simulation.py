cbe9e91b0c4c139ee885d3d93f85e47d4cf26c24
HiFiveJazz <jasmeet.bhatia.us@gmail.com>
2024-04-02 14:29:50 -0700

    created item simulation

--- item_simulation.py
+++ item_simulation.py
@@ -3,13 +3,54 @@ import random
 # green items -> 20%
 # white items -> 79%
 p_chest
-case p_chest >= 0.79 {
+case: p_chest >= 0.79 {
     for i in range(len(green_items))
 
 }
-case p_chest <= 0.01 {
+case: p_chest <= 0.01 {
     for i in range(len(red_item))
 }
-case p_chest > 0.01 && p_chest <0.79 {
+case: p_chest > 0.01 && p_chest <0.79 {
     for i in range(len(white_item))
+            item = random item
 }
+
+class Environment:
+    def __init__(self, 
+                 items,
+                 time,
+                 damage):
+
+        self.items = items 
+        self.time = time
+        self.damage = damage
+    
+class Difficulty:
+    def __init__(self, 
+                 items,
+                 time,
+                 damage):
+
+        self.items = items 
+        self.time = time
+        self.damage = damage
+
+difficulties = [Drizzle, Rainstorm, Monsoon, Eclipse]
+#Choose inital difficulty
+
+initial_difficulty 
+print("Choose a difficulty:")
+print("Drizzle (1), Rainstorm (2), or Monsoon (3)?")
+
+choice = 
+print("Are you playing multiplayer?")
+print("How many people are you playing with?")
+player_count
+stages_completed
+player_factor = 1 + 3 * (player_count - 1)
+time_factor = 0.0506 * difficulty_value * (player_count^0.2)
+stage_factor = (1.15^stages_completed)
+
+
+difficulty_coeffecient = (player_factor + time_factor + stage_factor)
+
