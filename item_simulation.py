7585ecca57958d5ebd44f6438211eea8775af62a
HiFiveJazz <jasmeet.bhatia.us@gmail.com>
2024-04-02 14:00:55 -0700

    begin item_simulation

---
+++ item_simulation.py
@@ -0,0 +1,15 @@
+import random 
+# red items -> 1% chance
+# green items -> 20%
+# white items -> 79%
+p_chest
+case p_chest >= 0.79 {
+    for i in range(len(green_items))
+
+}
+case p_chest <= 0.01 {
+    for i in range(len(red_item))
+}
+case p_chest > 0.01 && p_chest <0.79 {
+    for i in range(len(white_item))
+}
